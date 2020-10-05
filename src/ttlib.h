6bf347587524de3604159c521ebf96d165600cb7
grchibi <tomoyukit2@gmail.com>
2020-10-05 10:00:28 +0900

    1st commit

---
+++ src/ttlib.h
@@ -0,0 +1,21 @@
+/**
+ * ttlib.h
+ *
+ *    2020/10/03
+ */
+
+#include <string>
+#include <cstdio>
+#include <vector>
+
+template <typename ... Args>
+std::string format(const std::string& fmt, Args ... args)
+{
+	size_t len = std::snprintf(nullptr, 0, fmt.c_str(), args ...);
+	std::vector<char> buf(len + 1);
+	std::snprintf(&buf[0], len + 1, fmt.c_str(), args ...);
+	return std::string(&buf[0], &buf[0] + len);
+}
+
+
+// end of ttlib.h
